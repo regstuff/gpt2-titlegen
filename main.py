9a9cc4cc08103989dbb8b3faff71b83dc1d4b530
regstuff <6016831+regstuff@users.noreply.github.com>
2022-11-13 14:55:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ import gpt_2_simple as gpt2
 import os, requests, sys, json, urllib.parse
 from requests.structures import CaseInsensitiveDict
 from datetime import datetime
-import smtplib, ssl # For emailer
-from email.message import EmailMessage
-
-receiverid = os.environ['RECEIVERID']
-senderid = os.environ['SENDERID']
-mailpassword = os.environ['MAILPASS']
 
 token = os.environ['TOKEN']
 owner = os.environ['OWNER']
@@ -16,23 +10,6 @@ headers = CaseInsensitiveDict()
 headers["Accept"] = "application/vnd.github+json"
 headers["Authorization"] = "token " + token
 
-def sendmail(mailmessage):
-  if mailmessage != '':
-    context = ssl.create_default_context()
-    msg = EmailMessage()
-    msg['Subject'] = 'Reddit Jokes Update'
-    msg['To'] = receiverid 
-    msg['From'] = senderid
-    msg.set_content(mailmessage)
-    print(mailmessage)
-    #for receiverid in receiverids: # Send mail to all receiver ids
-    print('Sending email')
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login(senderid, mailpassword)
-        server.send_message(msg)
-        
-    print('Email sent')
-    
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess, run_name='Multichannel_SgVideosArticles_titles_keywords_v3')
 gen_file = 'gpt2_gentext.txt'
@@ -49,9 +26,6 @@ gpt2.generate_to_file(sess,
               top_p=0.9,
               destination_path=gen_file                      
               )
-
-with open(gen_file, 'r') as f: mailmsg = f.read()
-sendmail(mailmsg)
 
 open_api_url = f"https://api.github.com/repos/{owner}/gpt2-titlegen/issues" # Close the issue
 #data = '{"title":"today","body":'+'finalmsg'+'}'
