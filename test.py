06be39a57a8fbfbd10bb20c0d6b53c0bcf91f586
regstuff <6016831+regstuff@users.noreply.github.com>
2022-11-13 13:43:08 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import os
+
+receiverid = os.environ['RECEIVERID']
+receiverid = os.environ['SENDERID']
+mailpassword = os.environ['MAILPASS']
+
+token = os.environ['TOKEN']
+owner = os.environ['OWNER']
+
+print(receiverid[0], receiverid[0], token[0], owner[0], mailpassword[0])
